6932a996ed4b13fb2b81d6c3874cca1860f2f325
rizxddev <rizz994564@gmail.com>
2025-07-07 13:34:02 +0700

    Delete api/models/log.js

--- api/models/log.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-const logSchema = new mongoose.Schema({
-  time: String,
-  user: String,
-  action: String,
-  desc: String,
-});
-export default mongoose.models.Log || mongoose.model("Log", logSchema);
