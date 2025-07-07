d307a79d2cb09544a6474db990071dd63b3f0097
rizxddev <rizz994564@gmail.com>
2025-07-07 14:18:24 +0700

    Delete api directory

--- api/models/Admins.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-const adminSchema = new mongoose.Schema({
-  username: { type: String, unique: true },
-  email: String,
-  password: String, // hash di production!
-  role: { type: String, default: "admin" }
-});
-export default mongoose.models.Admin || mongoose.model("Admin", adminSchema);
