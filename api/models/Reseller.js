e56d8d0d682c08f19392ebcb4261991a1ce9f49c
rizxddev <rizz994564@gmail.com>
2025-07-07 13:33:51 +0700

    Delete api/models/Reseller.js

--- api/models/Reseller.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-const resellerSchema = new mongoose.Schema({
-  username: { type: String, unique: true },
-  password: String, // Saran: hash password di production!
-});
-export default mongoose.models.Reseller || mongoose.model("Reseller", resellerSchema);
