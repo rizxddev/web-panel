71d87b0a2fd0d4e89f880346e0a1f262ea7dda61
rizxddev <rizz994564@gmail.com>
2025-07-08 20:44:07 +0700

    Update create-panel.js

--- api/create-panel.js
+++ api/create-panel.js
@@ -71,7 +71,7 @@ export default async function handler(req, res) {
   const payload = {
     name: serverName,
     user: userId,
-    egg: 20,
+    egg: 15,
     docker_image: "ghcr.io/pterodactyl/yolks:nodejs_18",
     startup: "{{CMD_RUN}}",
     limits: {
